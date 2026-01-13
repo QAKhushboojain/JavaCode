9dba0c4666eef8ebeaf170f553f5d823af0de3e8
91946 <930khushboo.jain@gmail.com>
2026-01-13 19:44:45 +0530

    Array Day 2

---
+++ src/Array/Array_Day2.java
@@ -0,0 +1,9 @@
+package Array;
+
+public class Array_Day2 {
+    public static void main(String[] args) {
+        int a [] = new int[4];
+        System.out.println(a[0]);
+        // default int value in array = 0.
+    }
+}
