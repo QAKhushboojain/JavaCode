e05de3450f381307a1fc8a985012b47bbdad6810
91946 <930khushboo.jain@gmail.com>
2026-01-14 01:36:59 +0530

    Array Day 26 Array with Duplicates Frequency

---
+++ src/TwoPointers_SumPairs_Array/Array_Day26.java
@@ -0,0 +1,17 @@
+package TwoPointers_SumPairs_Array;
+
+public class Array_Day26 {
+    public static void main(String[] args) {
+        int a [] = {1,2,3,4,5};
+        int sum = 7;
+        for(int i = 0; i<a.length; i++)
+        {
+            for(int j = i+1; j<a.length; j++)
+            {
+                if(a[i]+ a[j] == sum)
+                    System.out.println(a[i] + " " + a[j]);
+                // Explanation: prints all pairs sum=7
+            }
+        }
+    }
+}
