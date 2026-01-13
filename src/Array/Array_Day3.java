d7432c680a7318626205c4014fc37fcf9ba4abee
91946 <930khushboo.jain@gmail.com>
2026-01-13 19:54:32 +0530

    Array Day 4

--- src/Array/Array_Day3.java
+++ src/Array/Array_Day3.java
@@ -4,5 +4,6 @@ public class Array_Day3 {
     public static void main(String[] args) {
         int a [] = {10,20,30};
         System.out.println(a[a.length-1]);
+        // Explanation: last index = length-1.
     }
 }
---
+++ src/Array/Array_Day4.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class Array_Day4 {
+    public static void main(String[] args) {
+        int a [] = {1,2,3,4,5};
+        for(int i = 0; i< a.length; i++)
+        {
+            System.out.println(a[i]);
+            // Explanation: standard forward loop.
+        }
+    }
+}
