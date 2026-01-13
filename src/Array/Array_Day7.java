e2392db1aee901092df876824ec3dac0293804b5
91946 <930khushboo.jain@gmail.com>
2026-01-13 21:43:34 +0530

    Array Day 7

---
+++ src/Array/Array_Day7.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class Array_Day7 {
+    public static void main(String[] args) {
+        int a [] = {1,2,3};
+        System.out.println(a[3]);
+        // Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 3 out of bounds for length 3
+        //Explanation: valid indices: 0..2.
+    }
+}
