c6b01095728cc8e7098396b760f9e0c7bd6bde91
91946 <930khushboo.jain@gmail.com>
2026-01-14 01:24:45 +0530

    Array Day 24 Array with Duplicates Frequency

---
+++ src/Sorting_OrderBasedOutputs_Array/Array_Day24.java
@@ -0,0 +1,22 @@
+package Sorting_OrderBasedOutputs_Array;
+
+public class Array_Day24 {
+    public static void main(String[] args) {
+        int a [] = {3,1,2};
+        java.util.Arrays.sort(a);
+        System.out.println(java.util.Arrays.toString(a));
+        // Explanation: ascending sort.
+        /* Step 2: Sort the array
+
+Arrays.sort() is a built-in Java method
+
+It sorts the array in ascending order
+
+Sorting is done in-place (no new array created)
+
+a = {1, 2, 3}
+
+
+         */
+    }
+}
