8308582a60b9d36f0c38ea2d20ad6b87a8f54d70
91946 <930khushboo.jain@gmail.com>
2026-01-13 22:12:34 +0530

    Array Day 11 Array with Loops and Index

---
+++ src/Loop_Index_Array/Array_Day11.java
@@ -0,0 +1,12 @@
+package Loop_Index_Array;
+
+public class Array_Day11 {
+    public static void main(String[] args) {
+        int a [] = {1,2,3,4,5};
+        for(int i = 0; i<a.length-1; i++)
+        {
+            System.out.println(a[i]);
+            // Explanation: a.length-1 =4 so loop i from 0 to 3.
+        }
+    }
+}
