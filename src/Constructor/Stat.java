0f73f3a1c710457fd628dadf8fe43f8410b3416e
91946 <930khushboo.jain@gmail.com>
2026-01-14 22:41:58 +0530

    Constructor Day - 28 Constructor and static initialization timing

---
+++ src/Constructor/Stat.java
@@ -0,0 +1,18 @@
+package Constructor;
+
+public class Stat {
+    static
+    {
+        System.out.println("Static");
+    }
+    Stat()
+    {
+        System.out.println("Constructor");
+    }
+
+    public static void main(String[] args) {
+        new Stat();
+        new Stat();
+        // Explanation: Static block runs once at class load; constructor runs each time.
+    }
+}
