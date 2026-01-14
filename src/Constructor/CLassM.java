fcddd65a6cb6d9f30a5f32b6389e0cf0981be729
91946 <930khushboo.jain@gmail.com>
2026-01-14 17:16:30 +0530

    Constructor Day - 15 Private constructor inaccessible from outside (compile-time)

---
+++ src/Constructor/CLassM.java
@@ -0,0 +1,11 @@
+package Constructor;
+
+public class CLassM {
+    // Private constructor inaccessible from outside (compile-time)
+    public static void main(String[] args) {
+       // ClassL l = new ClassL();
+        // Output
+        //Compile-time error: L() has private access in L
+        //Explanation: Cannot call private constructor from other class.
+    }
+}
