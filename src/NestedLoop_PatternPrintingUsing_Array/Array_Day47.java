ba61efa580807c4a936d5bdecb83b53847937096
91946 <930khushboo.jain@gmail.com>
2026-01-14 13:18:57 +0530

    Array Day 47 Nested Loop , Pattern Printing using array

---
+++ src/NestedLoop_PatternPrintingUsing_Array/Array_Day47.java
@@ -0,0 +1,15 @@
+package NestedLoop_PatternPrintingUsing_Array;
+
+public class Array_Day47 {
+    public static void main(String[] args) {
+
+        int[] a = {1, 2, 3};
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(a[j] + " ");  // ✅ print in same line
+            }
+            System.out.println(); // ✅ move to next line after each row
+        }
+    }
+}
