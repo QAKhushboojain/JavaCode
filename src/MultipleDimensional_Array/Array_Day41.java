441ab2c5734ad36f6e4142d5db60420f53036f7f
91946 <930khushboo.jain@gmail.com>
2026-01-14 11:32:40 +0530

    Array Day 41 Array with Multidimensional array

---
+++ src/MultipleDimensional_Array/Array_Day41.java
@@ -0,0 +1,12 @@
+package MultipleDimensional_Array;
+
+public class Array_Day41 {
+    public static void main(String[] args) {
+        int m [] [] = {{1,2,3}, {4,5,6}, {7,8,9}};
+        for(int i = 0; i<m.length; i++)
+        {
+            System.out.println(m[i][i]);
+            // Explanation: main diagonal elements.
+        }
+    }
+}
