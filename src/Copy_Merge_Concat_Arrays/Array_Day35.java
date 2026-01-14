bb2c8ac39792a5880f80934c6316f4ea667df5d6
91946 <930khushboo.jain@gmail.com>
2026-01-14 11:05:17 +0530

    Array Day 35 Array with Copy,Merge and Concat

---
+++ src/Copy_Merge_Concat_Arrays/Array_Day35.java
@@ -0,0 +1,10 @@
+package Copy_Merge_Concat_Arrays;
+
+public class Array_Day35 {
+    public static void main(String[] args) {
+        int a [] = {1,2,3};
+        int b [] = java.util.Arrays.copyOf(a, a.length);
+        System.out.println(java.util.Arrays.toString(b));
+        // Explanation: copyOf clones array.
+    }
+}
