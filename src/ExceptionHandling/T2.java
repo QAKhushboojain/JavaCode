962ad7d9423a1eb73925913e99a2ed60460fb517
91946 <930khushboo.jain@gmail.com>
2026-01-15 15:53:32 +0530

    Exception Handling - Day 2 ArrayIndexOutOfBoundsException

---
+++ src/ExceptionHandling/T2.java
@@ -0,0 +1,9 @@
+package ExceptionHandling;
+
+public class T2 {
+    public static void main(String[] args) {
+        int a [] = {1,2,3};
+        System.out.println(a[3]);
+        // Explanation: Valid indices 0..2; accessing index 3 throws array index exception.
+    }
+}
