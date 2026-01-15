2877d2b14d39efcd3be364ce29a047b118965b96
91946 <930khushboo.jain@gmail.com>
2026-01-15 15:57:28 +0530

    Exception Handling - Day 3 NullPointerException

---
+++ src/ExceptionHandling/T3.java
@@ -0,0 +1,9 @@
+package ExceptionHandling;
+
+public class T3 {
+    public static void main(String[] args) {
+        String s = null;
+        System.out.println(s.length());
+        // Explanation: Calling a method on null reference throws NullPointerException.
+    }
+}
