95f69d222bbcc6c2b7848179169e95e3ed6b32f0
91946 <930khushboo.jain@gmail.com>
2026-01-17 17:58:34 +0530

    Exception Handling - Day - 30 Exception when accessing file without handling (checked)

---
+++ src/ExceptionHandling/T30.java
@@ -0,0 +1,11 @@
+package ExceptionHandling;
+// Exception when accessing file without handling (checked)
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class T30 {
+    public static void main(String[] args) throws IOException {
+        new FileInputStream("No File.txt");
+        // FileNotFoundException is checked; declared thrown from main; JVM prints stack trace.
+    }
+}
