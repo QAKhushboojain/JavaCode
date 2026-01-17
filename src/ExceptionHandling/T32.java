fc9072ee0723373932e61e18943d3a7c69100256
91946 <930khushboo.jain@gmail.com>
2026-01-17 18:06:28 +0530

    Exception Handling - Day - 32 OutOfMemoryError (large array)

---
+++ src/ExceptionHandling/T32.java
@@ -0,0 +1,9 @@
+package ExceptionHandling;
+// OutOfMemoryError (large array)
+public class T32 {
+    public static void main(String[] args) {
+        int a [] = new int [Integer.MAX_VALUE];
+        //
+        //
+    }
+}
