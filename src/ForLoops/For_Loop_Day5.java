b4d28d52ae6944194327428a0539fef7d22047b9
91946 <930khushboo.jain@gmail.com>
2026-01-18 23:20:04 +0530

    For Loop - Day 5

---
+++ src/ForLoops/For_Loop_Day5.java
@@ -0,0 +1,11 @@
+package ForLoops;
+
+public class For_Loop_Day5 {
+    public static void main(String[] args) {
+        for(int i = 0; i<=3; i++)
+        {
+            System.out.println(i + "");
+            // Explanation: prints <= boundary (common confusion with arrays).
+        }
+    }
+}
