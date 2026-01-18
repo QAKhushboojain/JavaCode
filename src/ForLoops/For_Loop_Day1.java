45185be3397644270b21a106613395aa6c6bddd4
91946 <930khushboo.jain@gmail.com>
2026-01-18 22:57:33 +0530

    For Loop - Day 1

---
+++ src/ForLoops/For_Loop_Day1.java
@@ -0,0 +1,12 @@
+package ForLoops;
+
+public class For_Loop_Day1 {
+    public static void main(String[] args) {
+        int a [] = {1,2,3,4,5};
+        for(int i = 0; i<a.length; i++)
+        {
+            System.out.println(a[i] + "");
+            // Explanation: normal forward iteration.
+        }
+    }
+}
