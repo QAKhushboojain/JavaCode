c117219ace208ff2c7c5d082d630a589dca17813
91946 <930khushboo.jain@gmail.com>
2026-01-18 23:45:44 +0530

    For Loop - Day 14

---
+++ src/ForLoops/For_Loop_Day14.java
@@ -0,0 +1,13 @@
+package ForLoops;
+
+public class For_Loop_Day14 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i<3)
+        {
+            System.out.println(i);
+            i++;
+            // Explanation: standard while.
+        }
+    }
+}
