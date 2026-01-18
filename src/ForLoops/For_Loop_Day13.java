08cf0418cc8a1349b30eadb4601e7bc09bb73277
91946 <930khushboo.jain@gmail.com>
2026-01-18 23:44:17 +0530

    For Loop - Day 13

---
+++ src/ForLoops/For_Loop_Day13.java
@@ -0,0 +1,11 @@
+package ForLoops;
+
+public class For_Loop_Day13 {
+    public static void main(String[] args) {
+        for(int i=0,j=5;i<j;i++,j--)
+        {
+            System.out.println(i + "" + j);
+            // Explanation: two loop control variables — useful in pairwise iteration.
+        }
+    }
+}
