f46562f4dd38074f3a53dff9d13be00cceecac93
91946 <930khushboo.jain@gmail.com>
2026-01-18 23:17:26 +0530

    For Loop - Day 4

---
+++ src/ForLoops/For_Loop_Day4.java
@@ -0,0 +1,11 @@
+package ForLoops;
+
+public class For_Loop_Day4 {
+    public static void main(String[] args) {
+        int a [] = new int[3];
+        for(int i = 0; i<a.length; i++){
+            System.out.println(a[i] + "");
+            // Explanation: default int array values = 0.
+        }
+    }
+}
