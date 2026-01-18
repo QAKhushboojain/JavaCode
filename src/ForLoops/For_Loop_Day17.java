7bdf86fbdef3309bf8a349da449961eeb32fffa5
91946 <930khushboo.jain@gmail.com>
2026-01-18 23:50:14 +0530

    For Loop - Day 17

---
+++ src/ForLoops/For_Loop_Day17.java
@@ -0,0 +1,13 @@
+package ForLoops;
+
+public class For_Loop_Day17 {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i<3);
+        // Explanation: do-while executes body at least once.
+    }
+}
