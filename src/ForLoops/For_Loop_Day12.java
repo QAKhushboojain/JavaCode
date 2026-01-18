9d79a92a13d3dab89395d6ccfb54ddb3ade9aebe
91946 <930khushboo.jain@gmail.com>
2026-01-18 23:39:32 +0530

    For Loop - Day 12

---
+++ src/ForLoops/For_Loop_Day12.java
@@ -0,0 +1,11 @@
+package ForLoops;
+
+public class For_Loop_Day12 {
+    public static void main(String[] args) {
+        for(int i = 0; i<5;)
+        {
+            System.out.println(i++ + "");
+            // Explanation: increment inside body; loop header empty.
+        }
+    }
+}
