8279da900b9c3bac658b089f641b3567bdb7229c
91946 <930khushboo.jain@gmail.com>
2026-01-18 23:37:04 +0530

    For Loop - Day 11

---
+++ src/ForLoops/For_Loop_Day11.java
@@ -0,0 +1,12 @@
+package ForLoops;
+
+public class For_Loop_Day11 {
+    public static void main(String[] args) {
+        for (int i = 0; i<5; i++)
+        {
+            System.out.println(i + "");
+            i++;
+            // Explanation: i++ inside loop adds extra step.
+        }
+    }
+}
