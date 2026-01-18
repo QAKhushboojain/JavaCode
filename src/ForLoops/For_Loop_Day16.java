45c02c85ff1d196109beca0add09351b9df1e223
91946 <930khushboo.jain@gmail.com>
2026-01-18 23:48:31 +0530

    For Loop - Day 16

---
+++ src/ForLoops/For_Loop_Day16.java
@@ -0,0 +1,15 @@
+package ForLoops;
+
+public class For_Loop_Day16 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i<3)
+        {
+            System.out.println(i);
+            //    no increment
+            // (infinite loop)
+            //Explanation: missing increment → infinite loop (common bug).
+
+        }
+    }
+}
