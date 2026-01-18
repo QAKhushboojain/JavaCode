21074bf564c8a7ea0fb149ce22872dcc00806fa4
91946 <930khushboo.jain@gmail.com>
2026-01-18 23:04:50 +0530

    For Loop - Day 2

---
+++ src/ForLoops/For_Loop_Day2.java
@@ -0,0 +1,12 @@
+package ForLoops;
+
+public class For_Loop_Day2 {
+    public static void main(String[] args) {
+        for(int i = 0; i<5; i++)
+        {
+            System.out.println(i + " ");
+            // Explanation: i starts at 0.
+            // for(initialization; condition; increment)
+        }
+    }
+}
