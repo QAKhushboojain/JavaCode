c38ef47e50a337b0fb075665f2291edf8cd691ec
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:03:48 +0530

    IF - Else Day 27

---
+++ src/If_Else_Conditions/Day27.java
@@ -0,0 +1,14 @@
+package If_Else_Conditions;
+
+public class Day27 {
+    public static void main(String[] args) {
+        int n = -10;
+        if(n>0)
+        {
+            System.out.println("Positive");
+        }
+        else {
+            System.out.println("Negative");
+        }
+    }
+}
