4615d5400b7547c11c4e67b5deb99c6da0fe4236
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:26:11 +0530

    Day - 47

---
+++ src/If_Else_Conditions/Day47.java
@@ -0,0 +1,12 @@
+package If_Else_Conditions;
+
+public class Day47 {
+    public static void main(String[] args) {
+        String str = "Selenium";
+        if (str.contains("len"))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+
+    }
+}
