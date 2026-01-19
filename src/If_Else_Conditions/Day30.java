a21e7fff60bca9dab08f63e8a669cc3baf311234
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:08:18 +0530

    IF - Else Day 30

---
+++ src/If_Else_Conditions/Day30.java
@@ -0,0 +1,13 @@
+package If_Else_Conditions;
+
+public class Day30 {
+    public static void main(String[] args) {
+        int x = 10;
+        if(x > 5)
+        {
+            System.out.println("Hello");
+            System.out.println("World");
+            // Explanation: Only if controls one line; “World” executes always.
+        }
+    }
+}
