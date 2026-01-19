b2dfd2f9c30cbd7566681d911c847e82e37cda34
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:30:05 +0530

    Day - 51

---
+++ src/If_Else_Conditions/Day51.java
@@ -0,0 +1,13 @@
+package If_Else_Conditions;
+
+public class Day51 {
+    public static void main(String[] args) {
+        int i = 0;
+        if (i++ == 0)
+            if (i == 1)
+                System.out.println("Yes");
+            else
+                System.out.println("No");
+
+    }
+}
