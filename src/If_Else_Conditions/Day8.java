6796a964add75b33902aeb7f20eb3d8bf44d96d6
91946 <930khushboo.jain@gmail.com>
2026-01-19 22:15:41 +0530

    IF - Else Day 8

---
+++ src/If_Else_Conditions/Day8.java
@@ -0,0 +1,14 @@
+package If_Else_Conditions;
+
+public class Day8 {
+    public static void main(String[] args) {
+        int a = 5, b= 10, c= 15;
+        if(a<b && b<c)
+        {
+            System.out.println("Increasing order");
+        }
+        else {
+            System.out.println("Not Increasing");
+        }
+    }
+}
