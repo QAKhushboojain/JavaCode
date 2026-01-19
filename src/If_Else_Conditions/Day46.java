d82d208cc85cda4c905e79fef0282dd03bf46684
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:25:15 +0530

    Day - 46

---
+++ src/If_Else_Conditions/Day46.java
@@ -0,0 +1,12 @@
+package If_Else_Conditions;
+
+public class Day46 {
+    public static void main(String[] args) {
+        boolean flag = false;
+        if (flag = true)
+            System.out.println("Hello");
+        else
+            System.out.println("Hi");
+// Because flag = true assigns true → if(true) executes.
+    }
+}
