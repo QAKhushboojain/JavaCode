7ba0f4da471cc7f8aa1a55371947a114a322db3c
91946 <930khushboo.jain@gmail.com>
2026-01-19 18:03:46 +0530

    For Loop - Day 66

---
+++ src/ForLoops/For_Loop_Day66.java
@@ -0,0 +1,10 @@
+package ForLoops;
+
+public class For_Loop_Day66 {
+    public static void main(String[] args) {
+        for (int i = 0; ++i <= 3; ) {
+            System.out.println(i);
+        }
+//  Explanation: ++i increments before check.
+    }
+}
