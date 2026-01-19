9d4b4121fc23c1ed28d1e9773535f3c3e0937de6
91946 <930khushboo.jain@gmail.com>
2026-01-19 15:31:25 +0530

    For Loop - Day 52

---
+++ src/ForLoops/For_Loop_Day52.java
@@ -0,0 +1,11 @@
+package ForLoops;
+
+public class For_Loop_Day52 {
+    public static void main(String[] args) {
+        for(int i = 1; i>=1; i++)
+        {
+            System.out.println(i);
+            // Explanation: Condition i >= 1 always true → infinite loop.
+        }
+    }
+}
