0cb6e368e73c44dbe896d22e934d66782e1429b1
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:14:12 +0530

    IF - Else Day 36

---
+++ src/If_Else_Conditions/Day36.java
@@ -0,0 +1,11 @@
+package If_Else_Conditions;
+
+public class Day36 {
+    public static void main(String[] args) {
+     //   🧩 11️⃣ NOT (!) Operator
+        boolean flag = false;
+        if (!flag)
+            System.out.println("Flag is false");
+
+    }
+}
