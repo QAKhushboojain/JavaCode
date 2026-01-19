562f955f826bced51ebe22a4fa99ca3ac889c34f
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:18:29 +0530

    IF - Else Day 40

---
+++ src/If_Else_Conditions/Day40.java
@@ -0,0 +1,13 @@
+package If_Else_Conditions;
+
+public class Day40 {
+    public static void main(String[] args) {
+        String s1 = new String("Java");
+        String s2 = new String("Java");
+        if (s1.equals(s2))
+            System.out.println("Same");
+        else
+            System.out.println("Different");
+
+    }
+}
