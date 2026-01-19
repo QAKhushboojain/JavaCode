4b2c9bea2ec7f60fbbecde4ca400dfda174c450d
91946 <930khushboo.jain@gmail.com>
2026-01-19 17:35:20 +0530

    For Loop - Day 54

---
+++ src/ForLoops/For_Loop_Day54.java
@@ -0,0 +1,14 @@
+package ForLoops;
+
+public class For_Loop_Day54 {
+    public static void main(String[] args) {
+        for(int i = 1; i<=5 ; i++)
+        {
+            if (i==4)
+                break;
+            System.out.println(i);
+       //  Explanation: break stops the loop when i == 4.
+        }
+
+    }
+}
