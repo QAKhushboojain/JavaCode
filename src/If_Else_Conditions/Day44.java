f7354ff352edc20aedaa47602fa16e3ea5f0c614
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:22:50 +0530

    IF - Else Day 44

---
+++ src/If_Else_Conditions/Day44.java
@@ -0,0 +1,12 @@
+package If_Else_Conditions;
+
+public class Day44 {
+    public static void main(String[] args) {
+        int x = 10, y = 20, z = 30;
+        if (x < y && y < z)
+            System.out.println("Ascending");
+        else
+            System.out.println("Not Ascending");
+
+    }
+}
