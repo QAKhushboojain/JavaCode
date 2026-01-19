9d2a159154b966abdb24853ffc9ec9d3d562c748
91946 <930khushboo.jain@gmail.com>
2026-01-19 18:15:25 +0530

    For Loop - Day 72

---
+++ src/ForLoops/For_Loop_Day72.java
@@ -0,0 +1,11 @@
+package ForLoops;
+// Retry Mechanism Simulation
+public class For_Loop_Day72 {
+    public static void main(String[] args) {
+        int retryCount = 3;
+        for (int i = 1; i <= retryCount; i++) {
+            System.out.println("Attempt " + i);
+        }
+
+    }
+}
