bbe25802da4b96ef53a1636a83b9095e046ee3f5
91946 <930khushboo.jain@gmail.com>
2026-01-19 17:36:51 +0530

    For Loop - Day 55

---
+++ src/ForLoops/For_Loop_Day55.java
@@ -0,0 +1,12 @@
+package ForLoops;
+
+public class For_Loop_Day55 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i<=3)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
