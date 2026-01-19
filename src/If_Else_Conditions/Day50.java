b53a40741c10bc5ced27e8c5624d20469cce0c01
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:29:05 +0530

    Day - 50

---
+++ src/If_Else_Conditions/Day50.java
@@ -0,0 +1,14 @@
+package If_Else_Conditions;
+
+public class Day50 {
+    public static void main(String[] args) {
+        int retry = 2;
+        if (retry == 0)
+            System.out.println("No Retry");
+        else if (retry == 1)
+            System.out.println("Retry Once");
+        else
+            System.out.println("Multiple Retries");
+
+    }
+}
