d880e23a027841a4d4aac8dd756c2ec7476ea83b
91946 <930khushboo.jain@gmail.com>
2026-01-19 14:48:02 +0530

    For Loop - Day 32

---
+++ src/ForLoops/For_Loop_Day32.java
@@ -0,0 +1,14 @@
+package ForLoops;
+
+public class For_Loop_Day32 {
+    public static void main(String[] args) {
+        for(int i = 0; i<5; i++)
+        {
+            if (i==3)
+                break;
+            System.out.print(i + " ");
+
+            // Explanation: break exits loop early.
+        }
+    }
+}
