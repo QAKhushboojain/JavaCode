f2775a08dd90660890a0cd42346ea431a66bf434
91946 <930khushboo.jain@gmail.com>
2026-01-19 17:50:20 +0530

    For Loop - Day 60

---
+++ src/ForLoops/For_Loop_Day60.java
@@ -0,0 +1,11 @@
+package ForLoops;
+// Multiplication Table
+public class For_Loop_Day60 {
+    public static void main(String[] args) {
+    int n = 3;
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+
+    }
+}
