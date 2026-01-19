eba4cabf62548ffcb46dcc197e26c384c65945db
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:28:08 +0530

    Day - 49

---
+++ src/If_Else_Conditions/Day49.java
@@ -0,0 +1,12 @@
+package If_Else_Conditions;
+
+public class Day49 {
+    public static void main(String[] args) {
+        boolean isExecuted = false;
+        if (!isExecuted)
+            System.out.println("Execute Test Case");
+        else
+            System.out.println("Already Executed");
+
+    }
+}
