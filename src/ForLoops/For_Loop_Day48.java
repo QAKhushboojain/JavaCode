ca17315506f5bb20f17afc271eeb5e9a3a0b5529
91946 <930khushboo.jain@gmail.com>
2026-01-19 15:25:28 +0530

    For Loop - Day 48

---
+++ src/ForLoops/For_Loop_Day48.java
@@ -0,0 +1,10 @@
+package ForLoops;
+
+public class For_Loop_Day48 {
+    public static void main(String[] args) {
+        int a[] = null;
+        for(int i=0;i<a.length;i++)
+            System.out.println(i);
+// Explanation: null array reference.
+    }
+}
