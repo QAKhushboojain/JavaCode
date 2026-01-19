098880e5287d50e658a101b19cff8268efc19610
91946 <930khushboo.jain@gmail.com>
2026-01-19 15:28:17 +0530

    For Loop - Day 50

---
+++ src/ForLoops/For_Loop_Day50.java
@@ -0,0 +1,11 @@
+package ForLoops;
+
+public class For_Loop_Day50 {
+    public static void main(String[] args) {
+        for(int i = 1; i<=3; i++)
+        {
+            System.out.println(i);
+            // Explanation: i starts from 1 → prints till 3 → stops when i = 4 (condition false).
+        }
+    }
+}
