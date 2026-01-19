28e0f8ba368243d5c303957a22dd343f11e076f5
91946 <930khushboo.jain@gmail.com>
2026-01-19 17:56:16 +0530

    For Loop - Day 64

---
+++ src/ForLoops/For_Loop_Day64.java
@@ -0,0 +1,12 @@
+package ForLoops;
+
+public class For_Loop_Day64 {
+    public static void main(String[] args) {
+        int n = 4, fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        System.out.println(fact);
+
+    }
+}
