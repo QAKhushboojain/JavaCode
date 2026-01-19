7d8bdcd1b3afba98b66f8725a0e7f528365b4123
91946 <930khushboo.jain@gmail.com>
2026-01-19 19:00:03 +0530

    IF - Else Day 4

---
+++ src/If_Else_Conditions/Day4.java
@@ -0,0 +1,18 @@
+package If_Else_Conditions;
+
+public class Day4 {
+    public static void main(String[] args) {
+        int marks = 85;
+        if(marks>=90)
+        {
+            System.out.println("A+");
+        }
+        else  if (marks>=80){
+            System.out.println("A");
+        }
+        else
+        {
+            System.out.println("B");
+        }
+    }
+}
