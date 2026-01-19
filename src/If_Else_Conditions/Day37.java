7b05393ff281255731fe8ae513163a60c21c0c53
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:15:00 +0530

    IF - Else Day 37

---
+++ src/If_Else_Conditions/Day37.java
@@ -0,0 +1,10 @@
+package If_Else_Conditions;
+
+public class Day37 {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int max = (a > b) ? a : b;
+        System.out.println(max);
+
+    }
+}
