5832237f6088691e7026494f9d19bcec3f005d3d
91946 <930khushboo.jain@gmail.com>
2026-01-19 18:11:44 +0530

    For Loop - Day 69

---
+++ src/ForLoops/For_Loop_Day69.java
@@ -0,0 +1,11 @@
+package ForLoops;
+
+public class For_Loop_Day69 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i == 1) {
+            System.out.println("Hello");
+        }
+// Infinite loop (prints continuously).
+    }
+}
