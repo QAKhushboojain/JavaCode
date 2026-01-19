15d4a43e18d93cad701af2446378f72601990602
91946 <930khushboo.jain@gmail.com>
2026-01-19 13:32:44 +0530

    For Loop - Day 21

---
+++ src/ForLoops/For_Loop_Day21.java
@@ -0,0 +1,16 @@
+package ForLoops;
+
+public class For_Loop_Day21 {
+    public static void main(String[] args) {
+        for(int i = 0; i<3; i++)
+        {
+            for(int j = 0; j<3; j++)
+            {
+                if (i==j)
+                    break;
+                    System.out.print(i  +  "" +  j+ "");
+            }
+            System.out.println();
+        }
+    }
+}
