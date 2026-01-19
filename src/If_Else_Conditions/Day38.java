9fcc9a202d2f75967163accd211d8ae651ca869c
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:15:57 +0530

    IF - Else Day 38

---
+++ src/If_Else_Conditions/Day38.java
@@ -0,0 +1,9 @@
+package If_Else_Conditions;
+
+public class Day38 {
+    public static void main(String[] args) {
+        int n = 6;
+        System.out.println((n % 2 == 0) ? "Even" : "Odd");
+
+    }
+}
