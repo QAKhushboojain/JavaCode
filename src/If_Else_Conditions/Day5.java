7749521dea64f0bc0e0872efc88c4a280b44a8de
91946 <930khushboo.jain@gmail.com>
2026-01-19 22:10:44 +0530

    IF - Else Day 5

---
+++ src/If_Else_Conditions/Day5.java
@@ -0,0 +1,18 @@
+package If_Else_Conditions;
+
+public class Day5 {
+    public static void main(String[] args) {
+        int a = 5;
+        if(a>2)
+        {
+            System.out.println("A");
+        }
+        if(a>4){
+            System.out.println("B");
+        }
+        if(a>6){
+            System.out.println("C");
+            // Only conditions 1 and 2 are true.
+        }
+    }
+}
