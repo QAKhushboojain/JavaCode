a8fc3d090337e0df84b5b4305e9c5b36a866bd0f
91946 <930khushboo.jain@gmail.com>
2026-01-19 15:29:44 +0530

    For Loop - Day 51

---
+++ src/ForLoops/For_Loop_Day51.java
@@ -0,0 +1,10 @@
+package ForLoops;
+// Reverse Loop
+public class For_Loop_Day51 {
+    public static void main(String[] args) {
+for(int i = 3; i>=1; i--)
+{
+    System.out.println(i);
+}
+    }
+}
