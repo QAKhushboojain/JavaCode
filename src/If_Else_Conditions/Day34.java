bfc4c0e9e548aed3b5faa1acd1e628c50ad9fb88
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:12:30 +0530

    IF - Else Day 34

---
+++ src/If_Else_Conditions/Day34.java
@@ -0,0 +1,10 @@
+package If_Else_Conditions;
+
+public class Day34 {
+    public static void main(String[] args) {
+        int x = 5, y = 10;
+        if (x > 0 && y > 0)
+            System.out.println("Both Positive");
+
+    }
+}
