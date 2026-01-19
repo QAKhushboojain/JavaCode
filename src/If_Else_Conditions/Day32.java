602b31f4e408d26d09f32facf18ec6eae56385b7
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:10:31 +0530

    IF - Else Day 32

---
+++ src/If_Else_Conditions/Day32.java
@@ -0,0 +1,16 @@
+package If_Else_Conditions;
+
+public class Day32 {
+    public static void main(String[] args) {
+        int num = 25;
+        if (num > 0) {
+            if (num % 2 == 0)
+                System.out.println("Positive Even");
+            else
+                System.out.println("Positive Odd");
+        } else {
+            System.out.println("Negative");
+        }
+
+        }
+}
