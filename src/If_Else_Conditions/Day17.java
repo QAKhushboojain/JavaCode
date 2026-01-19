4072edbdef3d3b1766364963afce73d614298a8e
91946 <930khushboo.jain@gmail.com>
2026-01-19 22:38:09 +0530

    IF - Else Day 17

---
+++ src/If_Else_Conditions/Day17.java
@@ -0,0 +1,13 @@
+package If_Else_Conditions;
+
+public class Day17 {
+    public static void main(String[] args) {
+        boolean flag = false;
+        if (!flag){
+            System.out.println("Flage is False");
+        }
+        else {
+            System.out.println("Flag is True");
+        }
+    }
+}
