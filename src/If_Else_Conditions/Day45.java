a6b18210e4fcea9148808299cb728ae3917841a9
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:24:03 +0530

    IF - Else Day 45

---
+++ src/If_Else_Conditions/Day45.java
@@ -0,0 +1,12 @@
+package If_Else_Conditions;
+
+public class Day45 {
+    public static void main(String[] args) {
+        int x = 5;
+        if (x == 10)
+            System.out.println("True");
+        else
+            System.out.println("False");
+//  You can’t assign a value in if condition when type is int.
+    }
+}
