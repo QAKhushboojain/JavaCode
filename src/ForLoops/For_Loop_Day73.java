7dbaac188e4049d9edf3c7e38d28d6ef0ea8ec48
91946 <930khushboo.jain@gmail.com>
2026-01-19 18:19:21 +0530

    For Loop - Day 73

---
+++ src/ForLoops/For_Loop_Day73.java
@@ -0,0 +1,11 @@
+package ForLoops;
+
+public class For_Loop_Day73 {
+    public static void main(String[] args) {
+        int[] data = {10, 20, 30};
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i]);
+        }
+
+    }
+}
