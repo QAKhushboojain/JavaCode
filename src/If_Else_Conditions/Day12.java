eeb667c383e078f57255b86ea5d14897847d38fe
91946 <930khushboo.jain@gmail.com>
2026-01-19 22:23:17 +0530

    IF - Else Day 12

---
+++ src/If_Else_Conditions/Day12.java
@@ -0,0 +1,9 @@
+package If_Else_Conditions;
+
+public class Day12 {
+    public static void main(String[] args) {
+        int a  = 5, b = 10;
+        String result = (a>b) ? "A Greater" : "B Greater";
+        System.out.println(result);
+    }
+}
