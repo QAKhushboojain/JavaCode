727a77df7395b4a5e75550e6305fb7d5e5bb3429
91946 <930khushboo.jain@gmail.com>
2026-01-19 22:36:34 +0530

    IF - Else Day 16

---
+++ src/If_Else_Conditions/Day16.java
@@ -0,0 +1,12 @@
+package If_Else_Conditions;
+
+public class Day16 {
+    public static void main(String[] args) {
+        int x = 7;
+        if (x > 0 && x < 10)
+        {
+            System.out.println("Single Digit Positive");
+        }
+
+    }
+}
