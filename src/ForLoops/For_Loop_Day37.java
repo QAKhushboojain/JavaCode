99aee444f575b41da605939c436624a4e67aaadc
91946 <930khushboo.jain@gmail.com>
2026-01-19 14:59:38 +0530

    For Loop - Day 37

---
+++ src/ForLoops/For_Loop_Day37.java
@@ -0,0 +1,11 @@
+package ForLoops;
+
+public class For_Loop_Day37 {
+    public static void main(String[] args) {
+        while(true){
+            // nothing changes
+        }
+// Explanation: always true — common trap.
+//Advanced: loops with arithmetic side effects (modification of index variable)
+    }
+}
