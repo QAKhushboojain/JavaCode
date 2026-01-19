5217181660be2d26f604b676f857752e0baa7d83
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:01:03 +0530

    IF - Else Day 25

---
+++ src/If_Else_Conditions/Day25.java
@@ -0,0 +1,12 @@
+package If_Else_Conditions;
+
+public class Day25 {
+    public static void main(String[] args) {
+        int x = 10, y = 5;
+        if (x > y ? x < 20 : y < 10)
+            System.out.println("Condition True");
+        else
+            System.out.println("Condition False");
+
+    }
+}
