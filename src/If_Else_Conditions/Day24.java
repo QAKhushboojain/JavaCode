e23b943b832c5a16b1f711cc99c3d0e65760edba
91946 <930khushboo.jain@gmail.com>
2026-01-19 22:59:56 +0530

    IF - Else Day 24

---
+++ src/If_Else_Conditions/Day24.java
@@ -0,0 +1,13 @@
+package If_Else_Conditions;
+
+public class Day24 {
+    public static void main(String[] args) {
+        int a = 10;
+        if(a>5)
+        {
+            System.out.println("A");
+            System.out.println("B");
+            //  Second print not part of if (no braces).
+        }
+    }
+}
