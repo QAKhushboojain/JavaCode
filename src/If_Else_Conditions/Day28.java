18cbcaa15453d0c591dad78bc031dd3d7a3ad9ed
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:05:09 +0530

    IF - Else Day 28

---
+++ src/If_Else_Conditions/Day28.java
@@ -0,0 +1,14 @@
+package If_Else_Conditions;
+
+public class Day28 {
+    public static void main(String[] args) {
+        int a = 20, b = 30;
+        if(a>b)
+        {
+            System.out.println(a);
+        }
+        else {
+            System.out.println(b);
+        }
+    }
+}
