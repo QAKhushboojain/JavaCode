41d4380ef073fac20e2749fbcb5c3a7d0330a482
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:27:05 +0530

    Day - 48

---
+++ src/If_Else_Conditions/Day48.java
@@ -0,0 +1,12 @@
+package If_Else_Conditions;
+
+public class Day48 {
+    public static void main(String[] args) {
+        String status = "PASS";
+        if (status.equalsIgnoreCase("pass"))
+            System.out.println("Test Passed");
+        else
+            System.out.println("Test Failed");
+
+    }
+}
