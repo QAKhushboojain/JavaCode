944170da8b98379a5a860bc77882871fcd17bc7e
91946 <930khushboo.jain@gmail.com>
2026-01-19 17:40:45 +0530

    For Loop - Day 56

---
+++ src/ForLoops/For_Loop_Day56.java
@@ -0,0 +1,12 @@
+package ForLoops;
+
+public class For_Loop_Day56 {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Inside Loop");
+        }
+        while (i<5);
+        // Explanation: do-while runs body first, checks condition later.
+    }
+}
