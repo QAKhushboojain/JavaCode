92168dad99272d72fd1198634ae905ddac982352
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:02:31 +0530

    IF - Else Day 26

---
+++ src/If_Else_Conditions/Day26.java
@@ -0,0 +1,15 @@
+package If_Else_Conditions;
+
+public class Day26 {
+    public static void main(String[] args) {
+        int num = 7;
+        if(num % 2 == 0)
+        {
+            System.out.println("Even");
+        }
+        else {
+            System.out.println("Odd");
+            //  Explanation: 7 % 2 = 1 → condition false → prints Odd.
+        }
+    }
+}
