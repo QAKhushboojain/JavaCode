1ab25e7b010a9694bc30af6f9a03af362c10b900
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:11:42 +0530

    IF - Else Day 33

---
+++ src/If_Else_Conditions/Day33.java
@@ -0,0 +1,12 @@
+package If_Else_Conditions;
+
+public class Day33 {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age >= 18)
+            System.out.println("Eligible");
+        else
+            System.out.println("Not Eligible");
+
+    }
+}
