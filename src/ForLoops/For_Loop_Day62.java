e35df582aaf5c5c6a04e3c3cdfab7acb4d6f7275
91946 <930khushboo.jain@gmail.com>
2026-01-19 17:53:01 +0530

    For Loop - Day 62

---
+++ src/ForLoops/For_Loop_Day62.java
@@ -0,0 +1,11 @@
+package ForLoops;
+// Print Odd Numbers
+public class For_Loop_Day62 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 6; i++) {
+            if (i % 2 != 0)
+                System.out.print(i + " ");
+        }
+
+    }
+}
