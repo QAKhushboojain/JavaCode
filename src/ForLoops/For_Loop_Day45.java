bf4c6850573caaa35b881590c9992fa75a67de97
91946 <930khushboo.jain@gmail.com>
2026-01-19 15:19:43 +0530

    For Loop - Day 45

---
+++ src/ForLoops/For_Loop_Day45.java
@@ -0,0 +1,12 @@
+package ForLoops;
+
+public class For_Loop_Day45 {
+    public static void main(String[] args) {
+        String s = "abc";
+        for(int i = 0; i<s.length(); i++)
+        {
+            System.out.print(s.charAt(i) + " ");
+            // Explanation: charAt in loop.
+        }
+    }
+}
