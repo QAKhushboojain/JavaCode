644d3b91cc5c414d1957ebaa3b92d85389b1a929
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:21:58 +0530

    IF - Else Day 43

---
+++ src/If_Else_Conditions/Day43.java
@@ -0,0 +1,13 @@
+package If_Else_Conditions;
+
+public class Day43 {
+    public static void main(String[] args) {
+        int a = 5;
+        if (++a == 5)
+            System.out.println("True");
+        else
+            System.out.println("False");
+        System.out.println(a);
+
+    }
+}
