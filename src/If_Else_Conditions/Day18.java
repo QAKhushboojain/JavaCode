b8ce79200ba673ca639c6e5993235a55136f16c1
91946 <930khushboo.jain@gmail.com>
2026-01-19 22:51:07 +0530

    IF - Else Day 18

---
+++ src/If_Else_Conditions/Day18.java
@@ -0,0 +1,15 @@
+package If_Else_Conditions;
+
+public class Day18 {
+    public static void main(String[] args) {
+        int a = 10;
+        if(++a == 11)
+        {
+            System.out.println("Equal");
+        }
+        else {
+            System.out.println("Not Equal");
+            System.out.println(a);
+        }
+    }
+}
