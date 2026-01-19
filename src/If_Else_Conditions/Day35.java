c255e965cf63901670611802bcaa4783b35355a4
91946 <930khushboo.jain@gmail.com>
2026-01-19 23:13:17 +0530

    IF - Else Day 35

---
+++ src/If_Else_Conditions/Day35.java
@@ -0,0 +1,11 @@
+package If_Else_Conditions;
+
+public class Day35 {
+    public static void main(String[] args) {
+     //   🧩 🔟 OR (||) Operator
+        int a = -1, b = 3;
+        if (a > 0 || b > 0)
+            System.out.println("At least one positive");
+
+    }
+}
