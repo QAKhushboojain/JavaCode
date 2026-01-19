58cfb86089ebd6b0d135a10a1370c55f42370e27
91946 <930khushboo.jain@gmail.com>
2026-01-19 17:44:03 +0530

    For Loop - Day 57

---
+++ src/ForLoops/For_Loop_57.java
@@ -0,0 +1,13 @@
+package ForLoops;
+
+public class For_Loop_57 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 2; i++)
+        {
+            for (int j = 1; j <= 3; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
