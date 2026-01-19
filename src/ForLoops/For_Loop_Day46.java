e3d00a716ece370dd6f33c0f112b5080a0c0379f
91946 <930khushboo.jain@gmail.com>
2026-01-19 15:21:06 +0530

    For Loop - Day 46

---
+++ src/ForLoops/For_Loop_Day46.java
@@ -0,0 +1,10 @@
+package ForLoops;
+
+public class For_Loop_Day46 {
+    public static void main(String[] args) {
+        String s = "abc";
+        for(char c: s.toCharArray())
+            System.out.print(c + " ");
+// Explanation: for-each on char array.
+    }
+}
