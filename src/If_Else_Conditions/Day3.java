47b9ba91d5c0725cdfb820a987c2a28aa503f04f
91946 <930khushboo.jain@gmail.com>
2026-01-19 18:57:17 +0530

    IF - Else Day 3

---
+++ src/If_Else_Conditions/Day3.java
@@ -0,0 +1,15 @@
+package If_Else_Conditions;
+
+public class Day3 {
+    public static void main(String[] args) {
+        int a = 15;
+        if(a>10)
+        {
+            if(a<20)
+            {
+                System.out.println("Between 10 and 20");
+                // Both conditions true, inner block executes.
+            }
+        }
+    }
+}
