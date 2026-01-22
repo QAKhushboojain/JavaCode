da0f441257105c345473d5d49f07cb4e96852613
91946 <930khushboo.jain@gmail.com>
2026-01-23 01:22:08 +0530

    Inheritance Polymorphism Day - 36 Abstract method can't be private; compile error if attempted

---
+++ src/InheritancePolymorphism/A5.java
@@ -0,0 +1,7 @@
+package InheritancePolymorphism;
+
+abstract class A5 {
+ //    private abstract void m();
+    // Compile-time error: illegal combination of modifiers: abstract and private)
+    //Explanation: Abstract methods must be accessible to subclasses, cannot be private.
+}
