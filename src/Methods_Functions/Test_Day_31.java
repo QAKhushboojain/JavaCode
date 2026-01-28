23d232a0da3c285c52d886ca53f2b24e89a9cef3
91946 <930khushboo.jain@gmail.com>
2026-01-28 19:59:16 +0530

    Methods and Functions Day 32  Multiple return statements / execution flow

--- src/Methods_Functions/Test_Day_31.java
+++ src/Methods_Functions/Test_Day_31.java
@@ -4,6 +4,7 @@ public class Test_Day_31 {
     public static void main(String[] args) {
         B3 b=new B3();
         b.p();
+        // Explanation: private method in A isn't visible/overridden; B's p is independent.
     }
 }
 
---
+++ src/Methods_Functions/Test_Day_32.java
@@ -0,0 +1,15 @@
+package Methods_Functions;
+
+public class Test_Day_32 {
+    static int f(int x){
+        if(x>0) return 1;
+        return -1;
+    }
+    public static void main(String[] a){ System.out.println(f(5));
+    }
+}
+
+
+
+
+
