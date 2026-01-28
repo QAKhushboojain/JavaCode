4467f5c1d55ae6af642a7b133dffb72dfa838a2d
91946 <930khushboo.jain@gmail.com>
2026-01-28 11:10:27 +0530

    Inheritance Polymorphism Day - 51 Upcasting & method selection with generics removed (type erasure note)

---
+++ src/InheritancePolymorphism/Child25.java
@@ -0,0 +1,9 @@
+package InheritancePolymorphism;
+
+public class Child25 extends Parent25{
+    Parent25 p25  = new Child25();
+    //p25.m();
+    // Explanation: Simple runtime polymorphism example.
+}
+
+
---
+++ src/InheritancePolymorphism/Parent25.java
@@ -0,0 +1,8 @@
+package InheritancePolymorphism;
+
+public class Parent25 {
+    void m()
+    {
+        System.out.println("Parent25");
+    }
+}
