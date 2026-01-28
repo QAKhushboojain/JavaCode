a02d87039fbaa8130fd4a6cacf631f309975c988
91946 <930khushboo.jain@gmail.com>
2026-01-28 12:39:57 +0530

    Methods and Functions Day 1 and Day 2

---
+++ src/Methods_Functions/Test_Day_2.java
@@ -0,0 +1,13 @@
+package Methods_Functions;
+
+public class Test_Day_2 {
+    static void show()
+    {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        show();
+        // Explanation: void method prints.
+    }
+}
