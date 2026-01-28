748b7748dc917361dc02ba76d6336ed0552c4011
91946 <930khushboo.jain@gmail.com>
2026-01-28 11:01:55 +0530

    Inheritance Polymorphism Day - 49 Final class cannot be extended — compile error example

---
+++ src/InheritancePolymorphism/A7.java
@@ -0,0 +1,4 @@
+package InheritancePolymorphism;
+
+final class A7 {
+}
---
+++ src/InheritancePolymorphism/B6.java
@@ -0,0 +1,6 @@
+package InheritancePolymorphism;
+
+ //class B6 extends A7{
+     // (Compile-time error: cannot inherit from final A)
+     //Explanation: final class prevents inheritance.
+//}
