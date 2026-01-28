a0d9ff5decce1a00eecf394fb95f9b622d22ab2e
91946 <930khushboo.jain@gmail.com>
2026-01-28 19:43:17 +0530

    Methods and Functions Day 27

---
+++ src/Methods_Functions/Test_Day_27.java
@@ -0,0 +1,6 @@
+package Methods_Functions;
+
+public class Test_Day_27 {
+    // checked exception must be declared or handled:
+    // compile-time: void m() throws Exception { ... } or catch; else compile error
+}
