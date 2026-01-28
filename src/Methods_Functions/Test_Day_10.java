4177fbdd2083363afb9d371a0cef677c01252259
91946 <930khushboo.jain@gmail.com>
2026-01-28 14:23:42 +0530

    Methods and Functions Day 10

---
+++ src/Methods_Functions/Test_Day_10.java
@@ -0,0 +1,18 @@
+package Methods_Functions;
+
+public class Test_Day_10 {
+    static void a(Integer i)
+    {
+
+    }
+    static void a(Long l)
+    {
+
+    }
+
+    public static void main(String[] args) {
+      //  a(null);
+        // Compile-time error: reference to a is ambiguous
+        // Explanation: null matches both; ambiguous.
+    }
+}
