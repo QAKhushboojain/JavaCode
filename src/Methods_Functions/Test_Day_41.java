f190b88c92936f12b6eeec443961846226352acf
91946 <930khushboo.jain@gmail.com>
2026-01-29 10:09:26 +0530

    Methods and Functions Day 42 Synchronized method quick output (behavior same for simple examples)

---
+++ src/Methods_Functions/Test_Day_41.java
@@ -0,0 +1,12 @@
+package Methods_Functions;
+
+public class Test_Day_41 {
+    synchronized void s()
+    {
+        System.out.println("ok");
+    }
+
+    public static void main(String[] args) {
+        new Test_Day_41().s();
+    }
+}
