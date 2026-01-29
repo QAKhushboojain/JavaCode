4d0fd6eaed643c628dd27a8fbb35be17fba05b8f
91946 <930khushboo.jain@gmail.com>
2026-01-29 12:12:53 +0530

    Methods and Functions Day 48  main method signatures — multiple mains, which one runs

---
+++ src/Methods_Functions/Test_Day_47.java
@@ -0,0 +1,15 @@
+package Methods_Functions;
+
+public class Test_Day_47 {
+    public static void main(String[] args) {
+        System.out.println("Main1");
+    }
+    public static void main()
+    {
+        System.out.println("main2");
+    }
+    // Output when run normally
+    //main1
+    //Explanation: JVM uses public static void main(String[] args) signature.
+
+}
