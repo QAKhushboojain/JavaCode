c9ce67d5db38b67ec7c7fd4a0a4c470298b4129d
91946 <930khushboo.jain@gmail.com>
2026-01-31 14:55:16 +0530

    Operators Day -1

---
+++ src/Operators/Day_1.java
@@ -0,0 +1,13 @@
+package Operators;
+
+public class Day_1 {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println(i++);
+        System.out.println(i);
+        // Explanation: i++ returns old value (1) then increments to 2.
+        // i++ → print first, then add
+        //
+        //++i → add first, then print
+    }
+}
