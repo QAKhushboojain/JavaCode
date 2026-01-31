d890fc360fd53cfdb33d2d3beaebff767a6e0799
91946 <930khushboo.jain@gmail.com>
2026-01-31 20:25:16 +0530

    Operators Day - 43

---
+++ src/Operators/Day_43.java
@@ -0,0 +1,9 @@
+package Operators;
+
+public class Day_43 {
+    public static void main(String[] args) {
+        int x = 5;
+        x &= 3; // 0101 & 0011 = 0001
+        System.out.println(x);
+    }
+}
