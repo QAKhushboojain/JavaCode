42d6c23de58608f021c84e1a87c13420597dad65
91946 <930khushboo.jain@gmail.com>
2026-01-31 19:31:53 +0530

    Operators Day - 27 Precedence & evaluation order examples (left-to-right)

---
+++ src/Operators/Day_27.java
@@ -0,0 +1,11 @@
+package Operators;
+
+public class Day_27 {
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 2;
+        int k = i + j *2;
+        System.out.println(k);
+        // Explanation: * has higher precedence than +. Not an increment example but combine precedence awareness with operators.
+    }
+}
