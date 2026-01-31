afb25100ddbc44e295166e920779466319432ed2
91946 <930khushboo.jain@gmail.com>
2026-01-31 19:25:53 +0530

    Operators Day - 23 float/double

---
+++ src/Operators/Day_23.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class Day_23 {
+    public static void main(String[] args) {
+        float f = 1.0f;
+        f *= 2.5;
+        System.out.println(f);
+// Explanation: compound assignment handles type conversion.
+    }
+}
