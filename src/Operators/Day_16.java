2756aefcbea51520151e35f98daa2e59c6f59397
91946 <930khushboo.jain@gmail.com>
2026-01-31 19:13:25 +0530

    Operators Day - 16

---
+++ src/Operators/Day_16.java
@@ -0,0 +1,11 @@
+package Operators;
+
+public class Day_16 {
+    public static void main(String[] args) {
+        for(int i = 0; i<5; i+=2)
+        {
+            System.out.println(i + " ");
+            // Explanation: Compound-step increments.
+        }
+    }
+}
