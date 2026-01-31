51ec0533d88ebd5894729f4819da25b1f44de921
91946 <930khushboo.jain@gmail.com>
2026-01-31 19:09:02 +0530

    Operators Day - 14 ++/-- in loops (common patterns)

---
+++ src/Operators/Day_14.java
@@ -0,0 +1,11 @@
+package Operators;
+
+public class Day_14 {
+    public static void main(String[] args) {
+        for(int i = 0; i<5; i++)
+        {
+            System.out.println(i + " ");
+            // Explanation: Standard loop.
+        }
+    }
+}
