b8bf11f4b794873c68fe1865cbfd03afb7ca793b
91946 <930khushboo.jain@gmail.com>
2026-01-31 18:51:29 +0530

    Operators Day - 12

---
+++ src/Operators/Day_12.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class Day_12 {
+    public static void main(String[] args) {
+        int i = 1;
+        i = ++i;
+        System.out.println(i);
+        // Explanation: ++i increments then returns 2; i = 2.
+    }
+}
