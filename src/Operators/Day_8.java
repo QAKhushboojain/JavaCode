56dd96139eac27ab00212207f748309c6994578b
91946 <930khushboo.jain@gmail.com>
2026-01-31 17:09:52 +0530

    Operators Day - 8 String with +=

---
+++ src/Operators/Day_8.java
@@ -0,0 +1,10 @@
+package Operators;
+// String with +=
+public class Day_8 {
+    public static void main(String[] args) {
+        String s = "Hi";
+        s += 5;
+        System.out.println(s);
+        // Explanation: += concatenates when left operand is String.
+    }
+}
