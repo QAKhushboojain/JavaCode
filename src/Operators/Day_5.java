eaa6865eea8517c844039de5d990a472fd0fbc6f
91946 <930khushboo.jain@gmail.com>
2026-01-31 16:27:17 +0530

    Operators Day - 4

---
+++ src/Operators/Day_5.java
@@ -0,0 +1,12 @@
+package Operators;
+
+public class Day_5 {
+    public static void main(String[] args) {
+        int x = 5;
+        x += 3;
+        System.out.println(x);
+        // Explanation: x += 3 adds and assigns (same as x = x + 3 for int).
+    }
+
+
+}
