25f8da4b3ef84c5989de5a189c8dd4dfad551794
91946 <930khushboo.jain@gmail.com>
2026-01-31 19:27:17 +0530

    Operators Day - 24 ++ with char / Unicode

---
+++ src/Operators/Day_24.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class Day_24 {
+    public static void main(String[] args) {
+        char c = 'A';
+        c++;
+        System.out.println(c);
+      // Explanation: char behaves like integer for ++.
+    }
+}
