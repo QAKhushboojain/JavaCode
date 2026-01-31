f64ea2e3358d850e5bf6f67d2e89155b732ee010
91946 <930khushboo.jain@gmail.com>
2026-01-31 19:11:21 +0530

    Operators Day - 15

---
+++ src/Operators/Day_15.java
@@ -0,0 +1,11 @@
+package Operators;
+
+public class Day_15 {
+    public static void main(String[] args) {
+        for(int i = 0; i<5;)
+        {
+            System.out.println(i++ + " ");
+            // Explanation: Postfix in body increases i each iteration.
+        }
+    }
+}
