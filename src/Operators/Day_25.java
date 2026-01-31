a164f1ca6cdad501095e3a1de277bc77a62cb72a
91946 <930khushboo.jain@gmail.com>
2026-01-31 19:28:30 +0530

    Operators Day - 25

---
+++ src/Operators/Day_25.java
@@ -0,0 +1,9 @@
+package Operators;
+
+public class Day_25 {
+    public static void main(String[] args) {
+        char c = 'A';
+        System.out.println(++c);
+      // Explanation: prefix increments char
+    }
+}
