ca09e9c1ce2c3bfa68b8bd197dcf1af645983964
91946 <930khushboo.jain@gmail.com>
2026-01-31 20:23:44 +0530

    Operators Day - 41 and Day - 42

---
+++ src/Operators/Day_42.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class Day_42 {
+    public static void main(String[] args) {
+        int x = 5; // 0101
+        x <<= 1;
+        System.out.println(x);
+      // Explanation: left shift.
+    }
+}
