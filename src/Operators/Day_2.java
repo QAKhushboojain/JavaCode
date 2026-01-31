6063ac3ca26015b3dfe0580a39ef83e122d37077
91946 <930khushboo.jain@gmail.com>
2026-01-31 15:16:02 +0530

    Operators Day - 2 and Day - 3

---
+++ src/Operators/Day_2.java
@@ -0,0 +1,12 @@
+package Operators;
+
+public class Day_2 {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println(++i);
+        System.out.println(i);
+        // Expression	What happens
+        //i++	Use value first, then increment
+        //++i	Increment first, then use value
+    }
+}
---
+++ src/Operators/Day_3.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class Day_3 {
+    public static void main(String[] args) {
+        int i = 5;
+        System.out.println(i--);
+        System.out.println(i);
+        // Explanation: Postfix decrement returns old value, then decrements.
+    }
+}
