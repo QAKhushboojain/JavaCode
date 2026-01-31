22bc7d0a2767363ce49216f6a1f4d8962de693da
91946 <930khushboo.jain@gmail.com>
2026-01-31 16:23:45 +0530

    Operators Day - 2 and Day - 4

---
+++ src/Operators/Day_4.java
@@ -0,0 +1,13 @@
+package Operators;
+
+public class Day_4 {
+    public static void main(String[] args) {
+        int i = 5;
+        System.out.println(--i);
+        System.out.println(i);
+        // Explanation: Prefix decrement decrements first, then returns new value.
+        // --i → minus first, then print
+        //
+        //i-- → print first, then minus
+    }
+}
