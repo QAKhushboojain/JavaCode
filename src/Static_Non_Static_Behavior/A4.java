60a2169a9c9218761e050f785e146c0ad4e012ca
91946 <930khushboo.jain@gmail.com>
2026-02-13 14:56:52 +0530

    Day - 5 Instance method called using null reference (NPE)

---
+++ src/Static_Non_Static_Behavior/A4.java
@@ -0,0 +1,15 @@
+package Static_Non_Static_Behavior;
+
+public class A4 {
+    void n()
+    {
+        System.out.println("Instance");
+    }
+
+    public static void main(String[] args) {
+        A4 a = null;
+        a.n();
+        // Exception in thread "main" java.lang.NullPointerException
+        // Instance method called using null reference (NPE)
+    }
+}
