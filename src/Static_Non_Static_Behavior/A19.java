c79c766b8bcd8b97ff7f2b850ddcd4d66a892d31
91946 <930khushboo.jain@gmail.com>
2026-02-15 15:21:26 +0530

    Day - 25 static synchronized vs instance synchronized

---
+++ src/Static_Non_Static_Behavior/A19.java
@@ -0,0 +1,18 @@
+package Static_Non_Static_Behavior;
+// static synchronized vs instance synchronized
+public class A19 {
+    static synchronized void s()
+    {
+        System.out.println("Static Synchronized");
+    }
+    synchronized void i()
+    {
+        System.out.println("instance sync");
+    }
+
+    public static void main(String[] args) {
+        A19.s();
+        new A19().i();
+        // Explanation: both work single-threaded; static sync locks class object, instance sync locks instance.
+    }
+}
