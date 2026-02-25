f30adbd8f8fd10ab2344f3c4734d03f73bc4799f
91946 <930khushboo.jain@gmail.com>
2026-02-25 11:06:51 +0530

    Day - 2 Print all elements of an Array

---
+++ src/Array_Part_1/Day_2_Print_ArrayElements.java
@@ -0,0 +1,12 @@
+package Array_Part_1;
+
+public class Day_2_Print_ArrayElements {
+    public static void main(String[] args) {
+        int [] numbers = {5,10,15,20,25};
+        System.out.println("Array Elements Area");
+        for(int i = 0; i<numbers.length; i++)
+        {
+            System.out.println(numbers[i]);
+        }
+    }
+}
