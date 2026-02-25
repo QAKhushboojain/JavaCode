0eee1c9995348ba30981af1f6f4059c04a8fc5b9
91946 <930khushboo.jain@gmail.com>
2026-02-25 07:38:01 +0530

    Day -1 Declare and initialize an array

---
+++ src/Array_Part_1/Day_1_Array_Initialization.java
@@ -0,0 +1,11 @@
+package Array_Part_1;
+// Declare and initialize an array
+public class Day_1_Array_Initialization {
+    public static void main(String[] args) {
+        int [] numbers = {10,20,30,40,50};
+        for(int i = 0; i<numbers.length; i++)
+        {
+            System.out.println(numbers[i]);
+        }
+    }
+}
---
+++ src/Static_Non_Static_Behavior/Outer1.java
@@ -0,0 +1,15 @@
+package Static_Non_Static_Behavior;
+
+public class Outer1 {
+    int x = 5;
+    class Inner1{
+        void show()
+        {
+            System.out.println(x);
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
